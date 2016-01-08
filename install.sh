de074c223793c8a8d227be76ec29181ee5373b0b
4679 <btchina@live.com>
2016-01-08 19:05:07 +0800

    init

---
+++ install.sh
@@ -0,0 +1,25 @@
+#!/data/data/com.termux/files/usr/bin/bash
+
+termux-setup-storage
+apt update
+apt install -y git zsh
+git clone git://github.com/4679/oh-my-termux.git oh-my-termux
+clear
+
+if [ -d "$HOME/.termux" ]; then
+								mv $HOME/.termux $HOME/.termux.bak
+fi
+mv /data/data/com.termux/files/usr/etc/apt/sources.list /data/data/com.termux/files/usr/etc/apt/sources.list.bak
+
+mv oh-my-termux/.termux $HOME/.termux
+mv oh-my-termux/sources.list /data/data/com.termux/files/usr/etc/apt/sources.list
+
+termux-reload-settings
+
+apt update
+git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+cp $HONE/.oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
+chsh -s /data/data/com.termux/files/usr/bin/zsh
+zsh
+
+echo Done!
