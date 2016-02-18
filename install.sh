122fecb766a8ba1ffad50c105a2819d84e94d425
Jiawei Zhou <btchina@live.com>
2016-02-18 15:08:15 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 termux-setup-storage
+
+mv /data/data/com.termux/files/usr/etc/apt/sources.list /data/data/com.termux/files/usr/etc/apt/sources.list.bak
+mv oh-my-termux/sources.list /data/data/com.termux/files/usr/etc/apt/sources.list
+
 apt update
 apt install -y git zsh
 git clone git://github.com/4679/oh-my-termux.git oh-my-termux
@@ -9,17 +13,14 @@ clear
 if [ -d "$HOME/.termux" ]; then
  mv $HOME/.termux $HOME/.termux.bak
 fi
-mv /data/data/com.termux/files/usr/etc/apt/sources.list /data/data/com.termux/files/usr/etc/apt/sources.list.bak
 
 mv oh-my-termux/.termux $HOME/.termux
-mv oh-my-termux/sources.list /data/data/com.termux/files/usr/etc/apt/sources.list
-
-termux-reload-settings
 
 apt update
 git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
 cp $HOME/.oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
 chsh -s /data/data/com.termux/files/usr/bin/zsh
-zsh
 
 echo Done!
+
+exit
