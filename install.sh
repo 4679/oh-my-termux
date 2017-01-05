fbc4d19443c8aa76edbf1200067a61671230d1c6
4679 <btchina@live.com>
2017-01-05 11:33:27 +0800

    kiss it

--- install.sh
+++ install.sh
@@ -8,9 +8,8 @@ if [ -d "$HOME/.termux" ]; then
  mv $HOME/.termux $HOME/.termux.bak
 fi
 
-mkdir $HOME/.termux
-curl -fsSL https://cdn.rawgit.com/4679/oh-my-termux/master/.termux/colors.properties > $HOME/.termux/colors.properties
-curl -fsSL https://cdn.rawgit.com/4679/oh-my-termux/master/.termux/font.ttf > $HOME/.termux/font.ttf
+curl -fsLo $HOME/.termux/colors.properties --create-dirs https://cdn.rawgit.com/4679/oh-my-termux/master/.termux/colors.properties
+curl -fsLo $HOME/.termux/font.ttf --create-dirs https://cdn.rawgit.com/4679/oh-my-termux/master/.termux/font.ttf
 
 git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh --depth 1
 cp $HOME/.oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
