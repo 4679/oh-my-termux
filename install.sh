ba892d2a8020f05cb66c97a1f56bb84817291ca1
4679 <btchina@live.com>
2017-01-05 11:22:46 +0800

    updated

--- install.sh
+++ install.sh
@@ -1,25 +1,22 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-termux-setup-storage
-
-mv /data/data/com.termux/files/usr/etc/apt/sources.list /data/data/com.termux/files/usr/etc/apt/sources.list.bak
-curl -fsSL https://raw.githubusercontent.com/4679/oh-my-termux/master/sources.list > /data/data/com.termux/files/usr/etc/apt/sources.list
-
 apt update
 apt install -y git zsh
-git clone git://github.com/4679/oh-my-termux.git oh-my-termux
 clear
 
 if [ -d "$HOME/.termux" ]; then
  mv $HOME/.termux $HOME/.termux.bak
 fi
 
-mv oh-my-termux/.termux $HOME/.termux
+mkdir $HOME/.termux
+curl -fsSL https://cdn.rawgit.com/4679/oh-my-termux/master/.termux/colors.properties > $HOME/.termux/colors.properties
+curl -fsSL https://cdn.rawgit.com/4679/oh-my-termux/master/.termux/font.ttf > $HOME/.termux/font.ttf
 
-apt update
-git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh
+git clone git://github.com/robbyrussell/oh-my-zsh.git $HOME/.oh-my-zsh --depth 1
 cp $HOME/.oh-my-zsh/templates/zshrc.zsh-template $HOME/.zshrc
 chsh -s zsh
+
+termux-setup-storage
 
 echo Done!
 
